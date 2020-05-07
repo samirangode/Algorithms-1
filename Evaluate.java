4e22ba161135ae7af7d98b7ef45abd41e31b1beb
samirangode <samiran.gode.work@gmail.com>
2020-05-07 10:35:56 +0530

    Add files via upload

---
+++ Evaluate.java
@@ -0,0 +1,27 @@
+
+public class Evaluate {
+	public static void main(String args[]) {
+		Stack<String> ops = new Stack<String>();
+		Stack<Double> vals = new Stack<Double>();
+		
+		while(!StdIn.isEmpty()) {
+			String s = StdIn.readString();
+			if(s.equals("(")) ;
+			else if (s.equals("+"))
+				ops.push(s);
+			else if (s.equals("*"))
+				ops.push(s);
+			else if(s.equals(")")) {
+				String op = ops.pop();
+				//Double val1 = vals.pop();
+				//Double val2 = vals.pop();
+				if
+				(op.equals("+")) vals.push(vals.pop() + vals.pop());
+				else if (op.equals("*")) vals.push(vals.pop() * vals.pop());
+			}
+			
+			else vals.push(Double.parseDouble(s));
+			
+		}
+	}
+}
